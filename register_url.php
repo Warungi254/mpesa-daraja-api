435e15c929e72a5b694eb6d13c6124de9efee2df
Caroline Wanjiru <carolinewaruingu6@gmail.com>
2023-04-16 12:50:24 -0700

    Changes made

--- register_url.php
+++ register_url.php
@@ -3,8 +3,9 @@
 include 'accessToken.php';
 $registerurl = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
 $BusinessShortCode = '174379';
+//domain name for where all the transactions will be sent
 $confirmationUrl = 'https://methods-consultants.com/confirmation_url.php';
-$validationUrl = 'https://mydomain.com/validation_url.php';
+$validationUrl = 'https://methods-consultants.com/validation_url.php';
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $registerurl);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $access_token));
